a8eeadfca70dde87936a609a0ae85f058c366fce
Chiza Barber <chiza.alba@gmail.com>
2019-05-14 19:25:41 -0500

    working on adding favicon

--- src/helpers/renderer.js
+++ src/helpers/renderer.js
@@ -28,7 +28,6 @@ const renderer = (req, store, context) => {
                 ${helmet.title.toString()}
                 ${helmet.meta.toString()}
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
-                <link rel="shortcut icon" href="%PUBLIC_URL%/favicon.ico" />
                 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
                 <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
                 <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
