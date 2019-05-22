5f5f016b80c116d91b590abd20915e56a62d7791
Chiza Barber <chiza.alba@gmail.com>
2019-05-22 15:27:24 -0500

    express force https redirect

--- src/index.js
+++ src/index.js
@@ -20,6 +20,12 @@ app.use('/api', proxy(keys.redirectDomain, {
     }
 }))
 app.use(express.static('public'))
+app.use((req, res, next) => {
+    if (!req.secure && req.get('x-forwarded-proto') !== 'https' && process.env.NODE_ENV !== 'development') {
+        return res.redirect('https://' + req.get('host') + req.url)
+    }
+    next();
+})
 
 app.get('*', (req, res) => {
     const store = serverStore(req)
