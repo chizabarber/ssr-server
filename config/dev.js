538b7d878c1770175fdf52e7a2c82dc82395bea3
Chiza Barber <chiza.alba@gmail.com>
2019-06-07 13:25:56 -0500

    re-added dev.js for heroku

---
+++ config/dev.js
@@ -0,0 +1,11 @@
+// ------------------------------------------------------
+
+module.exports = {                                      // Bug: figure out how to push a successful build with dev.js in .gitignore
+    redirectDomain: 'http://localhost:5000',
+    currentDomain: 'localhost:3000',
+    faviconPath: '',
+    manifestPath: '',
+    environment: 'development'
+}
+
+// ------------------------------------------------------
