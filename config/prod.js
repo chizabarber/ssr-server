b54d79e3b5ffcb969931a3af488a7c3724de6ff3
Chiza Barber <chiza.alba@gmail.com>
2019-06-08 18:33:07 -0500

    config vars edit

--- config/prod.js
+++ config/prod.js
@@ -2,8 +2,8 @@
 
 module.exports = {
     currentDomain: process.env.CURRENT_DOMAIN,
-    faviconPath: process.env.FAVICON_PATH,
-    manifestPath: process.env.MANIFEST_PATH,
+    faviconPath: process.env.FAVICON_PATH || 'favicon.ico',
+    manifestPath: process.env.MANIFEST_PATH || 'manifest.json',
     redirectDomain: process.env.REDIRECT_DOMAIN,
     environment: 'production'
 }
